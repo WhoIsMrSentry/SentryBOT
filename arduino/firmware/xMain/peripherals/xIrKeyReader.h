d9c596c6744e49954f05107fbc688acef1490c70
SentryCoderDev <134494889+SentryCoderDev@users.noreply.github.com>
2026-01-27 10:02:32 +0300

    ir: remove verbose raw-hex debug prints and normalize constants

--- arduino/firmware/xMain/peripherals/xIrKeyReader.h
+++ arduino/firmware/xMain/peripherals/xIrKeyReader.h
@@ -26,29 +26,30 @@ public:
     outCode = IrReceiver.decodedIRData.decodedRawData;
     IrReceiver.resume();
     outKey = decodeToKey(outCode);
+    // Do not print raw IR debug output in normal operation.
     return true;
   }
 
 private:
   static String decodeToKey(uint32_t code){
     switch (code){
-      case 0xBA45FF00: return "1";
-      case 0xB946FF00: return "2";
-      case 0xB847FF00: return "3";
-      case 0xBB44FF00: return "4";
-      case 0xBF40FF00: return "5";
-      case 0xBC43FF00: return "6";
-      case 0xF807FF00: return "7";
-      case 0xEA15FF00: return "8";
-      case 0xF609FF00: return "9";
-      case 0xE916FF00: return "*";
-      case 0xE619FF00: return "0";
-      case 0xF20DFF00: return "#";
-      case 0xE718FF00: return "UP";
-      case 0xF708FF00: return "LEFT";
-      case 0xE31CFF00: return "OK";
-      case 0xA55AFF00: return "RIGHT";
-      case 0xAD52FF00: return "DOWN";
+      case 0xBA45FF00UL: return "1";
+      case 0xB946FF00UL: return "2";
+      case 0xB847FF00UL: return "3";
+      case 0xBB44FF00UL: return "4";
+      case 0xBF40FF00UL: return "5";
+      case 0xBC43FF00UL: return "6";
+      case 0xF807FF00UL: return "7";
+      case 0xEA15FF00UL: return "8";
+      case 0xF609FF00UL: return "9";
+      case 0xE916FF00UL: return "*";
+      case 0xE619FF00UL: return "0";
+      case 0xF20DFF00UL: return "#";
+      case 0xE718FF00UL: return "UP";
+      case 0xF708FF00UL: return "LEFT";
+      case 0xE31CFF00UL: return "OK";
+      case 0xA55AFF00UL: return "RIGHT";
+      case 0xAD52FF00UL: return "DOWN";
       default: return "UNKNOWN";
     }
   }
